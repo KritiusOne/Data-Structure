72279be1a4bd1e383c261ce7487599fa02b76900
Julio Rodríguez <julio10489@gmail.com>
2024-09-30 10:36:27 -0500

    Added: Use implemented functions in LinkendList files

--- program.cpp
+++ program.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
+#include "LinkendList.cpp"
+
 
 int main(){
-  std::cout << "Hola mundo" << std::endl;
+  Nodo head(1);
+  std::unique_ptr<Nodo> aux = std::make_unique<Nodo>(2);
+  head.next = std::move(aux);
+  printList(head);
   return 0;
 }
